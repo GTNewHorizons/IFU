7ecdc1ba6bf680af1f4410ba8a754286109e4617
Dream-Master <dream-master@gmx.net>
2018-06-08 21:09:54 +0200

    bump version

--- src/main/java/com/encraft/dz/lib/Reference.java
+++ src/main/java/com/encraft/dz/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	 public static final String MOD_NAME = "I Will Find You";
 	 public static final String MOD_ID = "ifu";
-	 public static final String MOD_VER = "0.9";
+	 public static final String MOD_VER = "1.0";
 	 public static final String CLIENT_PROXY = "com.encraft.dz.proxy.ClientProxy";
 	 public static final String	COMMON_PROXY = "com.encraft.dz.proxy.CommonProxy";
 
