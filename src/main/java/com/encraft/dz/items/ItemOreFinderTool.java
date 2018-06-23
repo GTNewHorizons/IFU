3afd7d586525bdb77bbc755e76dfe4db36a413b6
Richard Hendricks <richardhendricks@pobox.com>
2018-06-22 22:07:54 -0500

    Ore finder wand now searches on the server and passes results back to the client via the damage bar. Will now find GT ores that are not yet exposed/activated. Damage bar is now required. Searches above and below the character.  Searches are done by default every 250ms. Searches ignore GT background stone types.

--- src/main/java/com/encraft/dz/items/ItemOreFinderTool.java
+++ src/main/java/com/encraft/dz/items/ItemOreFinderTool.java
@@ -40,21 +40,19 @@ public class ItemOreFinderTool extends Item {
 
     private static Long lastUpdate = Long.valueOf(0L);
     private static Long millisPerUpdate = Long.valueOf(250L);
+    private static int MAX_DAMAGE = 10;
     private static boolean isClient;
-    private static int distanceMax;
     private static int toFind;
     private static String toFindStr;
     private static String toFindStr2;
     private static String configComment;
-    private static int distancePerLevel;
 
     private String[] dupe = cfg.blacklist;
     private static IIcon[] iconIndexes;
-    private static int distanceShortest = -1;
+    private static int tickcount = 0;
     private static int lastState = 0;
-    private static ArrayList found = new ArrayList();
+    private static int found = 0;
 
-    private static int[] vectorShortest = new int[3];
     private int id;
 
     Block oreBlock = null;
@@ -65,16 +63,10 @@ public class ItemOreFinderTool extends Item {
     public ItemOreFinderTool()
     {
         setUnlocalizedName(Reference.MOD_ID + "_" + "buildingKitItem");
-        setMaxDamage(100);
+        setMaxDamage(MAX_DAMAGE);
         setMaxStackSize(1);
         setTextureName(Reference.MOD_ID + ":meter0");
 
-        distanceMax = cfg.xzAreaRadius;
-
-        distancePerLevel = distanceMax / 4;
-        if (distancePerLevel < 1) {
-            distancePerLevel = 1;
-        }
     }
 
 
@@ -109,81 +101,91 @@ public class ItemOreFinderTool extends Item {
         return itemstack;
     }
 
-    @SideOnly(Side.CLIENT)
     public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) 
     {
-        if (!itemstack.hasTagCompound()) {
-            itemstack.setTagCompound(new NBTTagCompound());
+        if ((entity == null) || (world == null)) {
+            return;
         }
+        if (new Date().getTime() <= lastUpdate.longValue() ) { // Only search for ore blocks occasionally
+            return;
+        }
+        lastUpdate = Long.valueOf(new Date().getTime()) + millisPerUpdate.longValue();
 
-        if (ExtendedPlayer.get(Minecraft.getMinecraft().thePlayer).inventorybk.getStackInSlot(0) != null
-                && inventoryContainsAAD(Minecraft.getMinecraft().thePlayer.inventory) != null){
+        if(world.isRemote){ //Client side stuff
+            found = MAX_DAMAGE - itemstack.getItemDamage();
+            if( found < 0 ) { found = 0; }
+            
+            switch( found ) {
+                case 0:
+                    this.itemIcon = iconIndexes[0];
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    this.itemIcon = iconIndexes[1];
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    this.itemIcon = iconIndexes[2];
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    this.itemIcon = iconIndexes[3];
+                    break;
+                default:  // 10 or more found
+                    this.itemIcon = iconIndexes[4];
+                    break;
+            }
+            if (found >= (MAX_DAMAGE/2) && cfg.wandSound) {
+                world.playSound(entity.posX, entity.posY, entity.posZ,"ic2:tools.Treetap", 0.6F, 0.8F,true); //(Minecraft.getMinecraft().thePlayer, "ic2:tools.Treetap", 1.0F, 1.0F);
+            }
+        }
+        else { //Server side stuff
+            if (!itemstack.hasTagCompound()) {  /// Can this be removed?  Don't think this is used right now
+                itemstack.setTagCompound(new NBTTagCompound());
+            }
 
-            toFindStr = ExtendedPlayer.get(Minecraft.getMinecraft().thePlayer).inventorybk.getStackInSlot(0).getUnlocalizedName();
+            if ( !(entity instanceof EntityPlayer) ) {
+                return;
+            } 
+            ItemStack searchItem = ExtendedPlayer.get((EntityPlayer) entity).inventorybk.getStackInSlot(0);
+            if ( searchItem == null ) {
+                return;
+            }
+            if( inventoryContainsAAD( ((EntityPlayer) entity).inventory ) == null ) {
+                return;
+            }
+
+            toFindStr = searchItem.getUnlocalizedName();
             String splitFind[] = toFindStr.split("\\.");
-            if (splitFind[1].equals("blockores")) {
+            if (splitFind[1].equals("blockores")) { // Convert background materials to standard stone
                 short tMetaID = (short) (Integer.parseInt(splitFind[2]) % 1000);
                 toFindStr = splitFind[0] + "." + splitFind[1] + "." + tMetaID;
             }
-            if (new Date().getTime() <= lastUpdate.longValue()
-                    + millisPerUpdate.longValue()) {
-                return;
-            }
-            lastUpdate = Long.valueOf(new Date().getTime());
-            distanceShortest = -1;
 
-            if ((entity == null) || (world == null)) {
-                return;
-            }
-/*          Actually want to do server-side stuff, so don't check any more.
-            if (!entity.getClass().getSimpleName().equals("EntityClientPlayerMP")) {
-                return;
-            }
-*/
             double cur_x = entity.posX;
             double cur_y = entity.posY;
             double cur_z = entity.posZ;
 
-            int min_x = (int) cur_x - distanceMax - 1;
+            int min_x = (int) cur_x - cfg.xzAreaRadius - 1;
             int min_y = (int) cur_y - cfg.yAreaRadius;
-            int min_z = (int) cur_z - distanceMax;
+            int min_z = (int) cur_z - cfg.xzAreaRadius;
 
-            int max_x = (int) cur_x + distanceMax;
-            int max_y = (int) cur_y + distanceMax;
-            int max_z = (int) cur_z + distanceMax + 1;
+            int max_x = (int) cur_x + cfg.xzAreaRadius;
+            int max_y = (int) cur_y + cfg.yAreaRadius;
+            int max_z = (int) cur_z + cfg.xzAreaRadius + 1;
             boolean keepLooking = true;
+            found = 0;
 
             for (int z1 = min_z; (z1 < max_z) && (keepLooking); z1++) {
-                for (int x1 = min_x; x1 < max_x; x1++) {
-                    for (int y1 = min_y; y1 < max_y; y1++) {
-
+                for (int x1 = min_x; (x1 < max_x) && (keepLooking); x1++) {
+                    for (int y1 = min_y; (y1 < max_y) && (keepLooking); y1++) {
                         Block tBlock = world.getBlock(x1, y1, z1);
-
-                        if(!world.isRemote){
-                            //If running server side, update the block if it matches the ore so the client can "see" it.
-                            if (tBlock instanceof GT_Block_Ores_Abstract) {
-                                TileEntity tTileEntity = world.getTileEntity(x1,y1,z1);
-                                short tMetaID = (short)((GT_TileEntity_Ores) tTileEntity).getMetaData();
-                                String name = tBlock.getUnlocalizedName() + "." + (tMetaID % 1000);
-                                ((GT_TileEntity_Ores) tTileEntity).onUpdated();
-                                if (name.equals(toFindStr)) {
-                                    ((GT_TileEntity_Ores) tTileEntity).onUpdated();
-                                }
-                            }
-                            continue;
-                        }
-                        if (tBlock.getUnlocalizedName().equals(toFindStr)) {
-                            found.add(new int[] { x1, y1, z1 });
-                            //System.out.println("Found vanilla item");
-                            if (found.size() > 7 ){
-                                keepLooking = false;
-                            }
-                        }
-                        if (world.getBlock(x1, y1, z1).getUnlocalizedName().equals("gt.blockores")) {
-
-                            short tMetaID = (short)world.getBlockMetadata(x1, y1, z1);
-                            TileEntity tTileEntity = world.getTileEntity(x1, y1, z1);
-                            tMetaID = (short)((GT_TileEntity_Ores) tTileEntity).getMetaData();
+                        if (tBlock instanceof GT_Block_Ores_Abstract) {
+                            TileEntity tTileEntity = world.getTileEntity(x1,y1,z1);
+                            short tMetaID = (short)((GT_TileEntity_Ores) tTileEntity).getMetaData();
                             String name = tBlock.getUnlocalizedName() + "." + (tMetaID % 1000);
                             if (name.equals(toFindStr)) {
                                 boolean blacklisted = false;
@@ -194,60 +196,43 @@ public class ItemOreFinderTool extends Item {
                                         // ore is blacklisted, do not report
                                         blacklisted = true;
                                     }
-                                    if (!blacklisted ) {
-                                        found.add(new int[] { x1, y1, z1 });
-                                        //System.out.println("Exact match found");
-                                        if (found.size() > 7) {
-                                            keepLooking = false;
-                                        }
-                                        break;
+                                }
+                                if (!blacklisted ) {
+                                    found++;
+                                    //System.out.println("Exact match found");
+                                    if (found >= MAX_DAMAGE) { 
+                                        keepLooking = false;
                                     }
-                                }     //Disable whitelist checking for now
-                            }
-                            /* Disable crushed detection right now - it isn't working because getDrops is not working properly for GT ores.
-                            // MAybe needs to recurse
-                            System.out.println("Gregtech block ore found");
-                            ArrayList<ItemStack> drops = tBlock.getDrops(world, x1, y1, z1, tMetaID, 0);
-                            System.out.println( "drops.size() " + drops.size());
-                            for (int i = 0; i<drops.size();i++){
-                                System.out.println("Drop : drops.get(i).getUnlocalizedName().toString()" + drops.get(i).getUnlocalizedName().toString() );
-                                if ( drops.get(i).getUnlocalizedName().toString().equals(toFindStr)) {
-                                        System.out.println("Matching drop from ore");
-                                        for (String s :dupa) {
-                                        if (s != null && !s.equals( drops.get(i).getUnlocalizedName().toString())) {
-                                            found.add(new int[] { x1, y1, z1 });
-                                            System.out.println("whitelist match");
-                                            if (found.size() > 7) {
-                                                keepLooking = false;
-                                            }
-                                            break;
-                                        }
-                                    }    
+                                    continue;
                                 }
                             }
-                            */
+                        }
+                        
+                        if (tBlock.getUnlocalizedName().equals(toFindStr)) {
+                            boolean blacklisted = false;
+                            for (String s : dupe) {
+                                //System.out.println("Blacklisted ore " + s + " compared to " + name );
+                                if (s != null && s.equals(toFindStr)) {
+                                    //System.out.println("Ignore, ore is blacklisted");
+                                    // ore is blacklisted, do not report
+                                    blacklisted = true;
+                                }
+                            }
+                            if (!blacklisted ) {
+                                found++;
+                                //System.out.println("Exact match found");
+                                if (found >= MAX_DAMAGE) { 
+                                    keepLooking = false;
+                                }
+                                continue;
+                            }
                         }
                     }
                 }
             }
-        }else{
-            toFindStr = "Empty";
-            found.clear();
-        }
-        if(!world.isRemote){ return; } //Return if server-side
-        if (found.size() > 0 && found.size() <= 2) {this.itemIcon = iconIndexes[1];}
-        else if (found.size() >= 3 && found.size() <= 4) {this.itemIcon = iconIndexes[2];}
-        else if (found.size() >= 5 && found.size() <= 6) {this.itemIcon = iconIndexes[3];}
-        else if (found.size() >= 7 ) {this.itemIcon = iconIndexes[4];}
-        else if (found.size() <= 0){this.itemIcon = iconIndexes[0];}
-        if (cfg.dmgBar){itemstack.setItemDamage(found.size()+1);}
-        //System.out.println("You find: " + found.size());
-
-        if (found.size() >= 4 && cfg.wandSound) {
-            world.playSound(entity.posX, entity.posY, entity.posZ,"ic2:tools.Treetap", 0.6F, 0.8F,true); //(Minecraft.getMinecraft().thePlayer, "ic2:tools.Treetap", 1.0F, 1.0F);
+            itemstack.setItemDamage(MAX_DAMAGE - found);
         }
 
-        found.clear();
     }
 
     @SideOnly(Side.CLIENT)
