aff588068140ebda70e75b0c31d0c02138bc890a
Dream-Master <dream-master@gmx.net>
2018-10-30 19:38:32 +0100

    change Ore finder recipe again Tiny Mostly Visual Suggestion: Ore Finder Tech Themed #3799 https://github.com/GTNewHorizons/NewHorizons/issues/3799

--- src/main/java/com/encraft/dz/lib/Reference.java
+++ src/main/java/com/encraft/dz/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	 public static final String MOD_NAME = "I Will Find You";
 	 public static final String MOD_ID = "ifu";
-	 public static final String MOD_VER = "1.3";
+	 public static final String MOD_VER = "1.4";
 	 public static final String CLIENT_PROXY = "com.encraft.dz.proxy.ClientProxy";
 	 public static final String	COMMON_PROXY = "com.encraft.dz.proxy.CommonProxy";
 
