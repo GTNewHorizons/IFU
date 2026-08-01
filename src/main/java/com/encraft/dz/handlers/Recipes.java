6bfa6fd4694174e2585a34043d3ff3e34e59b399
Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
2026-08-01 01:29:28 -0500

    Restore ore dictionary ingredients in the building kit recipe

--- src/main/java/com/encraft/dz/handlers/Recipes.java
+++ src/main/java/com/encraft/dz/handlers/Recipes.java
@@ -1,10 +1,8 @@
 package com.encraft.dz.handlers;
 
-import com.ruling_0.materiallib.api.MaterialLibAPI;
-
 import gregtech.api.enums.GTValues;
+import gregtech.api.enums.OrePrefixes;
 import gregtech.api.enums.materials.Materials;
-import gregtech.api.enums.materials.Shapes;
 import gregtech.api.util.GTModHandler;
 
 public class Recipes implements Runnable {
@@ -14,12 +12,10 @@ public class Recipes implements Runnable {
                 GTModHandler.getModItem("ifu", "ifu_buildingKit", 1, GTValues.W),
                 GTModHandler.RecipeBits.BUFFERED | GTModHandler.RecipeBits.NOT_REMOVABLE
                         | GTModHandler.RecipeBits.DO_NOT_CHECK_FOR_COLLISIONS,
-                new Object[] { "XrX", "SGS", "RIR", 'X',
-                        MaterialLibAPI.getStack(Materials.IronMagnetic, Shapes.stick, 1), 'S',
-                        MaterialLibAPI.getStack(Materials.Wood, Shapes.stick, 1), 'G',
-                        MaterialLibAPI.getStack(Materials.Gold, Shapes.ring, 1), 'I',
-                        MaterialLibAPI.getStack(Materials.Iron, Shapes.stick, 1), 'R',
-                        MaterialLibAPI.getStack(Materials.Redstone, Shapes.dust, 1) });
+                new Object[] { "XrX", "SGS", "RIR", 'X', OrePrefixes.stick.ingredient(Materials.IronMagnetic), 'S',
+                        OrePrefixes.stick.ingredient(Materials.Wood), 'G', OrePrefixes.ring.ingredient(Materials.Gold),
+                        'I', OrePrefixes.stick.ingredient(Materials.Iron), 'R',
+                        OrePrefixes.dust.ingredient(Materials.Redstone) });
 
     }
 }
