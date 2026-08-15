311caa2a6544626fd1585b43c39e688aeb1eb768
boubou19 <miisterunknown@gmail.com>
2026-08-16 01:55:37 +0200

    Skip collisions in Recipes

--- src/main/java/com/encraft/dz/handlers/Recipes.java
+++ src/main/java/com/encraft/dz/handlers/Recipes.java
@@ -10,7 +10,8 @@ public class Recipes implements Runnable {
     public void run() {
         GTModHandler.addCraftingRecipe(
                 GTModHandler.getModItem("ifu", "ifu_buildingKit", 1, GTValues.W),
-                GTModHandler.RecipeBits.BUFFERED | GTModHandler.RecipeBits.NOT_REMOVABLE,
+                GTModHandler.RecipeBits.BUFFERED | GTModHandler.RecipeBits.NOT_REMOVABLE
+                        | GTModHandler.RecipeBits.DO_NOT_CHECK_FOR_COLLISIONS,
                 new Object[] { "XrX", "SGS", "RIR", 'X', OrePrefixes.stick.get(Materials.IronMagnetic), 'S',
                         OrePrefixes.stick.get(Materials.Wood), 'G', OrePrefixes.ring.get(Materials.Gold), 'I',
                         OrePrefixes.stick.get(Materials.Iron), 'R', OrePrefixes.dust.get(Materials.Redstone) });
